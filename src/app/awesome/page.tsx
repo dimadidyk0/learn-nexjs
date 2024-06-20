4e3ce00f7ef0973e16f5288fe82eea255a53743c
dimadidyk0 <dimadidyk0@gmail.com>
2024-06-20 09:03:59 +0200

    feat: add post component

--- src/app/awesome/page.tsx
+++ src/app/awesome/page.tsx
@@ -1,7 +1,12 @@
+
+import Post from "@components/Post";
+import React from "react";
+
 export default function AwesomePage() {
   return (
     <main>
       <h1>NextJS is awesome</h1>
+      <Post />
     </main>
   )
 }
