b76ba9f78896d81746d3158c8efa255f5ba21318
dimadidyk0 <dimadidyk0@gmail.com>
2024-06-21 13:38:26 +0200

    chore: 19. Outputting Dynamic Values

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+const names: string[] = ['Jack', 'Patrick'];
+
 export default function Post() {
+  const chosenName: string = Math.random() >= 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <p>Title</p>
+      <p>Name: {chosenName}</p>
       <p>Description</p>
     </div>
   )
