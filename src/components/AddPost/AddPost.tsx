257c5dc9860b5e815f3fb6706613370e0058bc65
dimadidyk0 <dimadidyk0@gmail.com>
2024-06-23 14:09:22 +0200

    lesson: add controlled inputs by using state

--- src/components/AddPost/AddPost.tsx
+++ src/components/AddPost/AddPost.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import s from './AddPost.module.css';
 
 export default function AddPost() {
+  const [name, setName] = useState('');
+  const [content, setContent] = useState('');
+
   return (
     <div className={s.root}>
       <form className={s.form}>
           <label className={s.label} htmlFor='content'>Text</label>
-          <textarea className={s.textarea} id="content" required />
+          <textarea
+            className={s.textarea}
+            id="content"
+            value={content}
+            required
+            onChange={e => setContent(e.target.value)}
+          />
+
           <label  className={s.label}htmlFor='name'>Name</label>
-          <input className={s.input} id="name" required />
+          <input
+            className={s.input}
+            id="name" required
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
 
           <button className={s.button}>submit</button>
       </form>
