b4b291c841d57e38ef4b8d9ce9672014579e8cef
dimadidyk0 <dimadidyk0@gmail.com>
2024-06-29 20:15:03 +0200

    lesson: add heade styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import logoImg from '@/assets/logo.png';
 import Link from 'next/link';
+import s from './Header.module.css';
 
 export default function Header() {
   return (
-    <header>
-      <Link href="/">
+    <header className={s.header}>
+      <Link className={s.logo} href="/">
         <img src={logoImg.src} alt="A plate with a food on it" />
         Next Level food
       </Link>
 
-      <nav>
+      <nav className={s.nav}> 
         <ul>
           <li>
             <Link href={"/meals"}>Browse Meals</Link>
