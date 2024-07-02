fa45fcb902dd09a8091ec120332b5b17b2b82726
dimadidyk0 <dimadidyk0@gmail.com>
2024-07-02 20:06:57 +0200

    lesson: use path in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
+"use client";
 import React from 'react';
 import logoImg from '@/assets/logo.png';
 import Link from 'next/link';
 import Image from 'next/image';
 //
 import s from './Header.module.css';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+  const path = usePathname();
+
   return (
     <header className={s.header}>
       <Link className={s.logo} href="/">
@@ -16,10 +20,10 @@ export default function Header() {
       <nav className={s.nav}> 
         <ul>
           <li>
-            <Link href={"/meals"}>Browse Meals</Link>
+            <Link href={"/meals"} className={path.startsWith("/meals") ? s.active : ''}>Browse Meals</Link>
           </li>
           <li>
-            <Link href={"/community"}>Foodies Community</Link>
+            <Link href={"/community"}  className={path.startsWith("/community") ? s.active : ''}>Foodies Community</Link>
           </li>
         </ul>
       </nav>
