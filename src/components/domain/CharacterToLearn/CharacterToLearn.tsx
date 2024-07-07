c5880aea98505f0c17dad44e31bb93410b8214f7
dimadidyk0 <dimadidyk0@gmail.com>
2024-07-07 15:23:02 +0200

    feat: add matarial icons and update character component

--- src/components/domain/CharacterToLearn/CharacterToLearn.tsx
+++ src/components/domain/CharacterToLearn/CharacterToLearn.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import React from 'react';
-import s from './CharacterToLearn.module.css';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 import { readme } from '@/services/readme';
+import s from './CharacterToLearn.module.css';
 
 type Props = {
   character: string;
 };
 
 export function CharacterToLearn({ character }: Props) {
-
   return (
-    <span onClick={() => readme(character)} className={s.character}>{character}</span>
+    <span className={s.character}>
+      {character}
+      <VolumeUpIcon onClick={() => readme(character)} className={s.soundIcon} />
+    </span>
   )
 };
