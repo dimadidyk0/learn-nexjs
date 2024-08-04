81a4afb5675b0fdab1a0de7dd256723187bb7838
dimadidyk0 <dimadidyk0@gmail.com>
2024-08-04 19:38:37 +0200

    chore: add more questions to hiragana exam and create templates for the future

--- src/app/test/[id]/exam.js
+++ src/app/test/[id]/exam.js
@@ -31,5 +31,180 @@ export const exam = {
       options: ['a','su','o','ka'], 
       correctIndex: 3,
     },
+    {
+      title: "き",
+      options: ['ki', 'sa', 'chi', 'i'], 
+      correctIndex: 0,
+    },
+    {
+      title: "く",
+      options: ['ko', 'ku', 'a', 'to'], 
+      correctIndex: 1,
+    },
+    {
+      title: "け",
+      options: ['ka', 'ki', 'ku', 'ke'], 
+      correctIndex: 3,
+    },
+    {
+      title: "こ",
+      options: ['ki', 'ko', 'so', 'o'], 
+      correctIndex: 1,
+    },
+    // {
+    //   title: "さ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "し",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "す",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "せ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "そ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "た",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "ち",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "つ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "て",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "と",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "な",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "に",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "ぬ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "ね",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "の",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "は",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "ひ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "ふ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "へ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "ほ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "ら",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "り",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "る",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "れ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "ろ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "や",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "ゆ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "よ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "わ",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "を",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
+    // {
+    //   title: "ん",
+    //   options: [], 
+    //   correctIndex: 0,
+    // },
   ],
 };
